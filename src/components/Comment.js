d8f7106b1477f4a34d6b097bab59a78b83776cb5
Piotr Łukomiak <cieplak32@gmail.com>
2016-12-05 12:25:10 +0100

    add Comment component

---
+++ src/components/Comment.js
@@ -0,0 +1,18 @@
+// @flow
+
+import React from 'react'
+import * as classNames from './Comment.scss'
+
+const Comment = ({ comment }) => (
+  <div className={classNames.comment}>
+    <div>
+      <span className={classNames.username}>{comment.user.username}</span>
+      <span className={classNames.date}>{comment.updatedAt}</span>
+    </div>
+    <div className={classNames.text}>
+      {comment.text}
+    </div>
+  </div>
+)
+
+export default Comment
