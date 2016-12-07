cf16799476b284f52875d0c7a8a2b64ef321c74c
Piotr Łukomiak <cieplak32@gmail.com>
2016-12-07 09:52:29 +0100

    add webpack relative path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf build
-NODE_ENV=production webpack --config webpack.config.js
+NODE_ENV=production node_modules/.bin/webpack --config webpack.config.js
 aws --region eu-central-1 s3 cp --recursive ./build s3://pioluk.me
