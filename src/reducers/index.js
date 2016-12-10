a3927cafd7099f5d1f3d3177732297c6a17be5e1
Piotr Łukomiak <cieplak32@gmail.com>
2016-12-10 21:10:19 +0100

    include comments reducer in root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,10 +6,12 @@ import ui from './ui'
 import auth from './auth'
 import events from './events'
 import notifications from './notifications'
+import comments from './comments'
 
 const rootReducer = combineReducers({
   auth,
   events,
+  comments,
   ui,
   notifications,
   routing: routerReducer
