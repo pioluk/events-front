3a9b3b47a30343da755f67d86b5bd4456fefccd0
Piotr Łukomiak <cieplak32@gmail.com>
2016-12-10 15:00:21 +0100

    extract config

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const API_URL = 'http://localhost:3000'
+export const EVENT_PAGE_SIZE = 24
+export const IMG_PREFIX_NANO = 'http://pioluk-event-images-processed.s3-website.eu-central-1.amazonaws.com/nano/'
+export const IMG_PREFIX_SMALL = 'http://pioluk-event-images-processed.s3-website.eu-central-1.amazonaws.com/small/'
+export const IMG_PREFIX_MEDIUM = 'http://pioluk-event-images-processed.s3-website.eu-central-1.amazonaws.com/medium/'
+export const IMG_PREFIX_LARGE = 'http://pioluk-event-images-processed.s3-website.eu-central-1.amazonaws.com/large/'
+export const IMG_PREFIX_XLARGE = 'http://pioluk-event-images-processed.s3-website.eu-central-1.amazonaws.com/xlarge/'
