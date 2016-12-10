00bf6552bd9d377a8b90d44cc52235a958af4c52
Piotr Łukomiak <cieplak32@gmail.com>
2016-12-10 15:17:56 +0100

    controlling deployment in deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $DEPLOY = false ]
+then
+  echo "Skipping deployment"
+  exit 0
+fi
+
 rm -rf build
 NODE_ENV=production node_modules/.bin/webpack --config webpack.config.js
 aws --region eu-central-1 s3 cp --recursive ./build s3://pioluk.me
