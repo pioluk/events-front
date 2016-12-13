2a4741c76f5333c8f14d437d93b40f666592c0b2
Piotr Łukomiak <cieplak32@gmail.com>
2016-12-13 21:57:15 +0100

    fix date/time pickers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import configureStore from './store/configureStore'
 import Root from './containers/Root'
@@ -12,6 +13,8 @@ import { loginSuccess } from './actions/auth'
 import { displayNotification } from './actions/notifications'
 
 import './index.css'
+
+injectTapEventPlugin()
 
 const store = configureStore({}, browserHistory)
 const history = syncHistoryWithStore(browserHistory, store)
