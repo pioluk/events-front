a5edb8b66ddf5caa3af5b2b55ca2f645574176f9
Piotr Łukomiak <cieplak32@gmail.com>
2016-12-15 18:14:45 +0100

    fix event tiles

--- src/components/EventTile.js
+++ src/components/EventTile.js
@@ -3,18 +3,18 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 import LazyImage from './LazyImage'
-import { IMG_PREFIX_NANO, IMG_PREFIX_XLARGE } from '../config'
+import { IMG_PREFIX_NANO, IMG_PREFIX_SMALL } from '../config'
 import { tile, overlay, text, title, date } from './EventTitle'
 import { formatDateTimeShort } from '../dates'
 
 const EventTile = ({ event, ...props }) => (
   <div className={tile} {...props}>
     <LazyImage
-      keepAspect={true}
+      keepAspect={false}
       height={158}
       color={'#' + event.color}
       small={IMG_PREFIX_NANO + event.image}
-      image={IMG_PREFIX_XLARGE + event.image} />
+      image={IMG_PREFIX_SMALL + event.image} />
     <div className={overlay}>
       <Link to={`/event/` + event.id}>
         <div className={text}>
