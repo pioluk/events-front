2f197b51da85a3da289ce471be01743f6a477f28
Piotr Łukomiak <cieplak32@gmail.com>
2016-12-17 15:16:32 +0100

    use cloudfront for images

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const API_URL = process.env.NODE_ENV === 'production' ? '//api.pioluk.me' : '//localhost:3000'
 export const EVENT_PAGE_SIZE = 24
 export const COMMENT_PAGE_SIZE = 50
-export const IMG_PREFIX_NANO = 'http://pioluk-event-images-processed.s3-website.eu-central-1.amazonaws.com/nano/'
-export const IMG_PREFIX_SMALL = 'http://pioluk-event-images-processed.s3-website.eu-central-1.amazonaws.com/small/'
-export const IMG_PREFIX_MEDIUM = 'http://pioluk-event-images-processed.s3-website.eu-central-1.amazonaws.com/medium/'
-export const IMG_PREFIX_LARGE = 'http://pioluk-event-images-processed.s3-website.eu-central-1.amazonaws.com/large/'
-export const IMG_PREFIX_XLARGE = 'http://pioluk-event-images-processed.s3-website.eu-central-1.amazonaws.com/xlarge/'
+export const IMG_PREFIX_NANO = 'https://images.pioluk.me/nano/'
+export const IMG_PREFIX_SMALL = 'https://images.pioluk.me/small/'
+export const IMG_PREFIX_MEDIUM = 'https://images.pioluk.me/medium/'
+export const IMG_PREFIX_LARGE = 'https://images.pioluk.me/large/'
+export const IMG_PREFIX_XLARGE = 'https://images.pioluk.me/xlarge/'
